574d9e52d036aa95f827ae3f9f9021d9dd6a2c01
Debojyoti Chatterjee <68095469+DebojyotiChatterjee321@users.noreply.github.com>
2021-04-08 17:51:00 +0530

    Change - 1

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../../model/recipe.model';
+
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+    recipes: Recipe[] = [
+        new Recipe('Test recipe', 'A simple description for my recipe', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe1%2FPasta_salad_closeup.JPG%2F1200px-Pasta_salad_closeup.JPG&f=1&nofb=1')
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
